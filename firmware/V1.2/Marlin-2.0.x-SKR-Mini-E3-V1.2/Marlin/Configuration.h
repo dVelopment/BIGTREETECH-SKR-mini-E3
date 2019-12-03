16db528e9903f91af99fc0b0d91dc77a0064c4e3
Daniel Holzmann <d@velopment.at>
2019-12-03 19:30:37 +0100

    add PID autotune results

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -477,9 +477,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 25.31
+  #define DEFAULT_Ki 2.24
+  #define DEFAULT_Kd 71.53
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
